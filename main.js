415f9cbc5ec3bbd2343c70bfcb24b38f0ae7aa51
Svyatoslafff <svjatoslavkom12@gmail.com>
2024-11-19 10:50:33 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+import './style.css'
+import { adding as sum, library } from '../my-library'
+// import * as libr from '../my-library'
+import validator from "validator" 
+// import { basicMaths } from 'basic-maths';
+
+const emailForm = {
+  form: document.querySelector(".email-submit-form"),
+  input: document.querySelector(".email-submit-form input"),
+  validateEmail(event) {
+    event.preventDefault();
+    if (validator.isEmail(emailForm.input.value)) {
+      console.log(emailForm.input.value);
+    } else {
+      alert(`${emailForm.input.value} isn't email`);
+    }
+  }
+};
+
+
+library.logSmth(sum(123, 5345, '223'), 'fsdfsdf', 43);
+// libr.library.logSmth("dasdas");
+
+emailForm.form.addEventListener('submit', emailForm.validateEmail);
