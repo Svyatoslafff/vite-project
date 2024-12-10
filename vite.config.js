782ab7b4fd5e8e1133d0b9747da3e2b4490639a4
Svyatoslafff <svjatoslavkom12@gmail.com>
2024-12-11 01:08:51 +0200

    vite-config fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,31 @@ import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
 	plugins: [injectHTML()],
+	build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
 });
